b019e8633541b6bdcad8ee67a8204ad6a2cde823
skitsy24 <mylesx24@gmail.com>
2025-10-26 13:10:46 -0700

    Finished unit testing #20

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, DateTime
 from datetime import datetime
-from database import Base
+from src.database import Base
 
 class Post(Base):
     __tablename__ = "posts"
